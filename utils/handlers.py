fa82872115c6cf5e8f836278d0b9e222bef3df8d
Danil Maximov <bazinga.mail@yandex.ru>
2020-05-21 01:11:49 +0300

    delete useless code. change users fields from bx24-api

--- utils/handlers.py
+++ utils/handlers.py
@@ -1,9 +1,7 @@
 import os
-from enum import Enum
 
 from dotenv import load_dotenv
 from bitrix24 import Bitrix24
-
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 if os.path.exists(dotenv_path):
@@ -13,54 +11,34 @@ bx24_token = os.environ.get('TOKEN')
 bx24_url = os.environ.get('URL')
 
 
-def create_deal(token: str, url: str, name: str, department: str) -> None:
+def create_deal(user_data, token=bx24_token, url=bx24_url) -> None:
     bx24 = Bitrix24(url, user_id=196)
+
+    full_name = user_data['full_name']
+    description = user_data['description']
+    phone_number = user_data['phone_number']
+    email = user_data['email']
+    department = int(user_data['department'])
+
     bx24.call_webhook('crm.deal.add', token, params={
         'fields': {
-            'CATEGORY_ID': None,
-            'UF_CRM_1588855684': 'NAME',
-            'UF_CRM_1588855793': None,
-            'UF_CRM_1588856215': 'Comment',
-            'UF_CRM_1588886392': 'example@example.com',
-            'UF_CRM_5BC6E8532C716': None,
+            'CATEGORY_ID': 27,
+            'UF_CRM_1588855684': full_name,
+            'UF_CRM_1588855793': department,
+            'UF_CRM_1588856215': description,
+            'EMAIL': email,
+            'PHONE': phone_number,
          },
     })
 
 
-class Department(Enum):
-    OFFICE = ('id', 'Офис')
-    OFFICE_ODESSKAYA = ('id', 'Офис(Одесская)')
-    IGNATOVA = ('id', 'Игнатова')
-    KAZBEKSKAYA = ('id', 'Казбекская')
-    KOTLYAROVA = ('id', 'Котлярова')
-    KRASNAYA = ('id', 'Красная')
-    KP = ('id', 'Красная-площадь')
-    LOFT = ('id', 'Лофт')
-    MEGA = ('id', 'Мега')
-    NOVOROS_MOLODEJ = ('id', ' Новороссийск Молодежная')
-    NOVOROS_SEREB = ('id', 'Новороссийск Серебрякова')
-    POKRISHKINO = ('id', 'Покрышкина')
-    SBS = ('id', 'СБС')
-    STAVROPOLSKAYA = ('id', 'Ставропольская')
-    TUAPSE = ('id', 'Туапсе')
-    TULYAEVA = ('id', 'Тюляева')
-    CHECKISTOV = ('id', 'Чекистов')
-    ROSSIYSKAYA = ('id', 'Российская')
-
-    def __init__(self, id, title):
-        self.id = id
-        self.title = title
-
-    @classmethod
-    def get_all(cls) -> list:
-        departments = [currency.title for currency in cls]
-        return departments
-
-    @classmethod
-    def get_choices(cls) -> tuple:
-        choices = tuple((currency.id, currency.title) for currency in cls)
-        return choices
+def _call_custom_command(token=bx24_token, url=bx24_url):
+    bx24 = Bitrix24(url, user_id=196)
+    result = bx24.call_webhook('crm.contact.fields', token, params={
+        'ID': 27
+    })
+    print(result)
 
 
 if __name__ in '__main__':
-    create_deal(token=bx24_token, url=bx24_url)
+    _call_custom_command()
